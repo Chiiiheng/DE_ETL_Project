06fd598c1995e6282d32d936de5620f5c918c507
Chiiiheng <jchjiangcheng@outlook.com>
2024-08-14 20:39:13 +0100

    update

--- godata2023/pipelines/transform.py
+++ godata2023/pipelines/transform.py
@@ -1,33 +1,34 @@
 import json
-import os
 from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Dict, List, Optional
 from pyspark.sql.window import Window
-from delta.tables import DeltaTable
 from pyspark.sql import DataFrame, SparkSession
-from pyspark.sql.functions import (col, current_timestamp, expr, lit,first,last,
-                                   monotonically_increasing_id, to_date, sum,max,min,desc,
+from pyspark.sql.functions import (col, current_timestamp, expr, lit, first, last,
+                                   monotonically_increasing_id, to_date, sum, max, min, desc,
                                    to_timestamp)
 from pyspark.sql.types import (DoubleType, IntegerType, StringType,
                                StructField, StructType, TimestampType, DateType, LongType)
+
 
 def trade_volume_aggregator(df):
     """
     aggregate trade data
     """
     df = df.withColumn('trade_date', to_date("trade_timestamp"))
-    window = Window.partitionBy('trade_date','symbol').orderBy('trade_date')
+    window = Window.partitionBy('trade_date', 'symbol').orderBy('trade_date')
     df = df.withColumn('daily_volume', sum('volume').over(window))
     df = df.withColumn('daily_high', max('high').over(window))
     df = df.withColumn('daily_low', min('low').over(window))
 
-    window_open = Window.partitionBy('trade_date','symbol').orderBy('trade_timestamp')
-    window_close = Window.partitionBy('trade_date','symbol').orderBy(desc('trade_timestamp'))
+    window_open = Window.partitionBy(
+        'trade_date', 'symbol').orderBy('trade_timestamp')
+    window_close = Window.partitionBy(
+        'trade_date', 'symbol').orderBy(desc('trade_timestamp'))
     df = df.withColumn('daily_open', first("open").over(window_open))
     df = df.withColumn('daily_close', first("close").over(window_close))
-    daily_trade=df.select("trade_date", "symbol","daily_volume", "daily_high", "daily_low", "daily_open",
-                        "daily_close").distinct().orderBy("trade_date")
+    daily_trade = df.select("trade_date", "symbol", "daily_volume", "daily_high", "daily_low", "daily_open",
+                            "daily_close").distinct().orderBy("trade_date")
     daily_trade = df.select(
         df['trade_date'].cast(DateType()),
         df['symbol'].cast(StringType()),
@@ -36,10 +37,11 @@ def trade_volume_aggregator(df):
         df['daily_high'].cast(DoubleType()).alias('high'),
         df['daily_low'].cast(DoubleType()).alias('low'),
         df['daily_close'].cast(DoubleType()).alias('close')
-        ).distinct().orderBy("trade_date")
-    
-    batch_id=current_timestamp().cast("long")
-    daily_trade = daily_trade.withColumn("partition",col("trade_date").cast('string'))
+    ).distinct().orderBy("trade_date")
+
+    batch_id = current_timestamp().cast("long")
+    daily_trade = daily_trade.withColumn(
+        "partition", col("trade_date").cast('string'))
     daily_trade = daily_trade.withColumn("batch_id", lit(batch_id))
 
     return daily_trade
@@ -56,14 +58,15 @@ def stock_events_normalizer(data: dict, spark: SparkSession):
 
     enrich_events = []
     for date, dict_value in body.items():
-        dict_value['symbol'] = dict_value.get('symbol',symbol)
-        dict_value['trade_timestamp'] = dict_value.get('trade_timestamp',date)
+        dict_value['symbol'] = dict_value.get('symbol', symbol)
+        dict_value['trade_timestamp'] = dict_value.get('trade_timestamp', date)
         enrich_events.append(dict_value)
-       
+
     df_raw = spark.createDataFrame(enrich_events)
     # type casting
     df = df_raw.select(
-        df_raw["trade_timestamp"].cast(TimestampType()).alias("trade_timestamp"),
+        df_raw["trade_timestamp"].cast(
+            TimestampType()).alias("trade_timestamp"),
         df_raw["symbol"].cast(StringType()).alias("symbol"),
         df_raw["`1. open`"].cast(DoubleType()).alias("open"),
         df_raw["`2. high`"].cast(DoubleType()).alias("high"),
@@ -72,9 +75,9 @@ def stock_events_normalizer(data: dict, spark: SparkSession):
         df_raw["`5. volume`"].cast(IntegerType()).alias("volume")
     )
 
-    batch_id=current_timestamp().cast("long")
+    batch_id = current_timestamp().cast("long")
     df = df.withColumn("partition", to_date("trade_timestamp").cast("string"))
     df = df.withColumn("batch_id", lit(batch_id))
-     # Show the DataFrame for testing
+    # Show the DataFrame for testing
     # df.show()
     return df
